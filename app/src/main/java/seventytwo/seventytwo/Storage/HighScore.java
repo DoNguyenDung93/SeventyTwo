441e9b27cf2281eac0d24b472f28d20c84bafacd
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-10 21:51:52 +0800

    Added HighScore and LastGameState classes in the Storage package.

---
+++ app/src/main/java/seventytwo/seventytwo/Storage/HighScore.java
@@ -0,0 +1,8 @@
+package seventytwo.seventytwo.Storage;
+
+/**
+ * Created by dongu on 10/12/2015.
+ * This class contains the highest score.
+ */
+public class HighScore {
+}
---
+++ app/src/main/java/seventytwo/seventytwo/Storage/LastGameState.java
@@ -0,0 +1,8 @@
+package seventytwo.seventytwo.Storage;
+
+/**
+ * Created by dongu on 10/12/2015.
+ * This class remembers the last game state so that the user will be able to continue the last game.
+ */
+public class LastGameState {
+}
