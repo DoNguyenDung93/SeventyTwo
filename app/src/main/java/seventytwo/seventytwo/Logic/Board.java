e9f9e07e1f9df516e872800720b190f69c1a8da7
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-10 23:16:05 +0800

    Update Board classes with details.

--- app/src/main/java/seventytwo/seventytwo/Logic/Board.java
+++ app/src/main/java/seventytwo/seventytwo/Logic/Board.java
@@ -2,11 +2,26 @@ package seventytwo.seventytwo.Logic;
 
 /**
  * Created by dongu on 10/12/2015.
- * This class will handle the creation and manipulation of the game board, which consists of 72 squares.
- * Each square will fit one token.
+ * This class will handle the creation and manipulation of the game board, which consists of 72 cells.
+ * Each cell will fit one token.
  */
 public class Board {
 
-    // Attributes
+    private static Board singleBoard = new Board();
 
+    // Attributes
+    private int[] row = {1, 2, 3, 4, 5, 6};
+    private int[] column = {1, 2, 3, 4, 5, 6};
+
+    // Constructor
+    public Board() {
+    }
+
+    // Accessor
+    public static Board getInstance() {
+        if (singleBoard == null) {
+            singleBoard = new Board();
+        }
+        return singleBoard;
+    }
 }
--- app/src/main/java/seventytwo/seventytwo/Logic/Cell.java
+++ app/src/main/java/seventytwo/seventytwo/Logic/Cell.java
@@ -2,7 +2,7 @@ package seventytwo.seventytwo.Logic;
 
 /**
  * Created by dongu on 10/12/2015.
- * This class handles square, which is an individual unit on the game board.
+ * This class handles cell, which is an individual unit on the game board.
  */
 public class Cell {
     // Attributes
