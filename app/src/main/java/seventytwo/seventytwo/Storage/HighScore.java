1e866b27243edc5525e5c743b1f6784ae27efb73
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-11 23:18:24 +0800

    Added method to access the save file and return the high score.

--- app/src/main/java/seventytwo/seventytwo/Storage/HighScore.java
+++ app/src/main/java/seventytwo/seventytwo/Storage/HighScore.java
@@ -1,8 +1,39 @@
 package seventytwo.seventytwo.Storage;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by dongu on 10/12/2015.
  * This class contains the highest score.
  */
 public class HighScore {
+    // Attribute
+    private File _saveFile;
+
+    // Constructor
+
+    public HighScore() {
+    }
+
+    public int openSaveFile() throws IOException {
+        String fileName = "save.txt";
+        _saveFile = new File(fileName);
+        int highScore = 0;
+
+        boolean doesFileExist = doesFileExist(_saveFile);
+
+        if (!doesFileExist) {
+            _saveFile.createNewFile();
+            // TODO
+        } else {
+            // TODO
+        }
+
+        return highScore;
+    }
+
+    private boolean doesFileExist(File file) {
+        return file.exists();
+    }
 }
