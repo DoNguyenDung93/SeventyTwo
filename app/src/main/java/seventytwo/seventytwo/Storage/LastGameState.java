ed1ee328abbf5367ebc40a4e3479eeb6cd6ee12e
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-11 23:11:59 +0800

    Added method to acces the save file and return the state of the board.

--- app/src/main/java/seventytwo/seventytwo/Storage/LastGameState.java
+++ app/src/main/java/seventytwo/seventytwo/Storage/LastGameState.java
@@ -1,8 +1,42 @@
 package seventytwo.seventytwo.Storage;
 
+import java.io.File;
+import java.io.IOException;
+
+import seventytwo.seventytwo.Component.Board;
+
 /**
  * Created by dongu on 10/12/2015.
  * This class remembers the last game state so that the user will be able to continue the last game.
  */
 public class LastGameState {
+
+    // Attribute
+    private File _saveFile;
+
+    // Constructor
+
+    public LastGameState() {
+    }
+
+    public Board openSaveFile() throws IOException {
+        String fileName = "save.txt";
+        _saveFile = new File(fileName);
+        Board board = new Board();
+
+        boolean doesFileExist = doesFileExist(_saveFile);
+
+        if (!doesFileExist) {
+            _saveFile.createNewFile();
+            // TODO
+        } else {
+            // TODO
+        }
+
+        return board;
+    }
+
+    private boolean doesFileExist(File file) {
+        return file.exists();
+    }
 }
