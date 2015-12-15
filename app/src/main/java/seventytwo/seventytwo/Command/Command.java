73a0a33f019cd586fa9e1941554d2233c5cd0434
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-15 22:39:20 +0800

    Added abstract Command class.

---
+++ app/src/main/java/seventytwo/seventytwo/Command/Command.java
@@ -0,0 +1,9 @@
+package seventytwo.seventytwo.Command;
+
+/**
+ * Created by dongu on 15/12/2015.
+ * This is an abstract class that is inherited by other command classes.
+ */
+public class Command {
+
+}
