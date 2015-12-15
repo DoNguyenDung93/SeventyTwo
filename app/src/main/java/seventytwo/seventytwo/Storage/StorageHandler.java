0b67df83183508337cf0dcd00978454381ae7c79
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-15 23:12:35 +0800

    Deleted HighScore and LastGameState classes. Add StorageHandler and StorageManipulator classes.

---
+++ app/src/main/java/seventytwo/seventytwo/Storage/StorageHandler.java
@@ -0,0 +1,8 @@
+package seventytwo.seventytwo.Storage;
+
+/**
+ * Created by dongu on 15/12/2015.
+ * This class handles the writing and reading of save file.
+ */
+public class StorageHandler {
+}
---
+++ app/src/main/java/seventytwo/seventytwo/Storage/StorageManipulator.java
@@ -0,0 +1,8 @@
+package seventytwo.seventytwo.Storage;
+
+/**
+ * Created by dongu on 15/12/2015.
+ * This class handles the temporary storage which
+ */
+public class StorageManipulator {
+}
