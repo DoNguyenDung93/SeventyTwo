62a2c6e606c505df0b50f438bc7e58c34443a145
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-12-16 16:49:03 +0800

    Added several methods to the Storage package.

--- app/src/main/java/seventytwo/seventytwo/Storage/StorageHandler.java
+++ app/src/main/java/seventytwo/seventytwo/Storage/StorageHandler.java
@@ -1,6 +1,7 @@
 package seventytwo.seventytwo.Storage;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -30,6 +31,8 @@ public class StorageHandler {
 
         if (!doesFileExist) {
             _storage.createNewFile();
+            saveContent.add("0");
+            saveContent.add("0");
             _logger.log(Level.INFO, "Create a new save file and return empty high score and last game state.");
         } else {
             Scanner sc = new Scanner(_storage);
@@ -44,5 +47,21 @@ public class StorageHandler {
 
     private boolean doesFileExist(File file) {
         return file.exists();
+    }
+
+    public void transferTempStorageToFile(ArrayList<String> saveContent) throws IOException {
+        FileWriter file = new FileWriter(_storage);
+        copyAllContentToFile(file, saveContent);
+        file.close();
+        _logger.log(Level.INFO, "Copy all save contents into save file.");
+    }
+
+    private void copyAllContentToFile(FileWriter file, ArrayList<String> saveContent) throws IOException {
+        for (int i = 0; i < saveContent.size(); i++) {
+            String content = saveContent.get(i);
+            file.write(content);
+            file.write("\n");
+            file.flush();
+        }
     }
 }
--- app/src/main/java/seventytwo/seventytwo/Storage/StorageManipulator.java
+++ app/src/main/java/seventytwo/seventytwo/Storage/StorageManipulator.java
@@ -1,8 +1,49 @@
 package seventytwo.seventytwo.Storage;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seventytwo.seventytwo.Logger.GlobalLogger;
+
 /**
  * Created by dongu on 15/12/2015.
  * This class handles the temporary storage which
  */
 public class StorageManipulator {
+
+    private ArrayList<String> _saveContent;
+    private StorageHandler _storageHandler;
+    private static Logger _logger = GlobalLogger.getInstance().getLogger();
+
+    // Constructor
+    public StorageManipulator() {
+        _storageHandler = new StorageHandler();
+    }
+
+    // Accessors
+    public ArrayList<String> getSaveContent() {
+        return _saveContent;
+    }
+
+    // Mutators
+    public void setSaveContent(ArrayList<String> saveContent) {
+        _saveContent = saveContent;
+    }
+
+    public void generateStorageManipulator() throws IOException {
+        String fileName = "save.txt";
+        _saveContent = _storageHandler.openSaveFile();
+        _logger.log(Level.INFO, "Generate the temporary storage with the save content.");
+    }
+
+    public void saveTempStorage(ArrayList<String> saveContent) {
+        for (int i = 0; i < _saveContent.size(); i++) {
+            _saveContent.set(i, saveContent.get(i));
+        }
+        _logger.log(Level.INFO, "Overwrite the save content in the temp storage.");
+    }
+
+
 }
